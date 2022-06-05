41bcb69c26e233e5ad356554ea9db721fee45777
JurelUwU <102762316+JurelUwU@users.noreply.github.com>
2022-06-04 19:47:17 -0500

    Add files via upload

---
+++ js/rblModel/rblModel.js
@@ -0,0 +1,10 @@
+//Modelo1 cambios de imagen
+function imageReset(){
+    document.getElementById("modelo1").src="../modelos/modelo1.webp";
+}
+function imageCottonPink(){
+    document.getElementById("modelo1").src="../Modelo-RBL/imgColorsHair/Modelo1-RBL/Cotton Pink.png";
+}
+function imageSparkOragen(){
+    document.getElementById("modelo1").src="../Modelo-RBL/imgColorsHair/Modelo1-RBL/Spark Orange.png";
+}
---
+++ js/rblModel/rblModelo3.js
@@ -0,0 +1,45 @@
+//Modelo1 cambios de imagen
+function imageReset(){
+    document.getElementById("modelo3").src="../modelos/modelo3.webp";
+}
+
+//Neon
+function imageCottonPink(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Cotton_Pink.webp";
+}
+
+function imagePastelCottonPink(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Pastel_Cotton_Pink.webp";
+}
+
+function imageSparkOragen(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Spark_Orange.webp";
+}
+
+function imagePastelSparkOragen(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Pastel_Spark_Orange.webp";
+}
+
+function imageLizard(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Lizard.webp";
+}
+
+/*Falta imagen.
+function imagePastelLizard(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/PastelLizard.webp";
+}
+*/
+
+function imageBanana(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Banana.webp";
+}
+
+function imagePastelBanana(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Pastel_Banana.webp";
+}
+
+function imageKiwi(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Kiwi.webp";
+}
+
+
