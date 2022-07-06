7e254ce73f07ea49bb66bf5079c30c965ac9e2fb
JurelUwU <102762316+JurelUwU@users.noreply.github.com>
2022-07-06 04:11:28 -0500

    Add files via upload

--- js/rblModel/rblModelo3.js
+++ js/rblModel/rblModelo3.js
@@ -5,41 +5,110 @@ function imageReset(){
 
 //Neon
 function imageCottonPink(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Cotton_Pink.webp";
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/NEON ALGODON COTTON PINK.webp";
 }
 
-function imagePastelCottonPink(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Pastel_Cotton_Pink.webp";
-}
 
 function imageSparkOragen(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Spark_Orange.webp";
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/NEON CHISPA NARANJA.webp";
 }
 
-function imagePastelSparkOragen(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Pastel_Spark_Orange.webp";
-}
 
 function imageLizard(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Lizard.webp";
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/NEON VERDE LIZARD.webp";
 }
 
-/*Falta imagen.
-function imagePastelLizard(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/PastelLizard.webp";
-}
-*/
 
 function imageBanana(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Banana.webp";
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/NEON AMARILLO BANANA.webp";
 }
 
-function imagePastelBanana(){
-    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Pastel_Banana.webp";
-}
 
 function imageKiwi(){
     document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/Kiwi.webp";
 }
 
 
+//Green Colors
+function imageEmerald(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/ESMERALDA-.webp";
+}
+
+
+function imageEnvyGreen(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/VERDE ENVIDIA.webp";
+}
+
+//Blue Colors
+function imageMagicBlued(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/AZUL MAGICO.webp";
+}
+
+function imageDeepBlue(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/AZULPROFUNDO .webp";
+}
+
+function imageElectricBlue(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/AZUL ELECTRICO.webp";
+} 
+
+//Pink Colors
+function imageConfetti(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/CONFETTI.webp";
+}
+
+function imageRebelPink(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/ROSA REBELDE.webp";
+}
+
+function imageCandyPink(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/ROSA.webp";
+}
+
+//PURPLE COLORS
+function imagePurple(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/PURPURA.webp";
+}
+
+function imageGrave(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/UVA.webp";
+}
+
+// Red Colors
+function imageRubyRed(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/ROJO RUBI.webp";
+}
+
+function imageBloodyRed(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/ROJO SANGRE.webp";
+}
+
+function imageAtomicRed(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/ROJO ATOMICO.webp";
+}
+
+//Violet Colors
+function imageectricViolet(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/VIOLETA ELECTRICO.webp";
+}
+
+//PASTEL COLORS
+function imagePolarGrey(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/PASTEL GRIS POLAR.webp";
+}
+
+function imageOpal(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/ROSA OPALO.webp";
+}
+
+function imageLilac(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/PASTEL LILA.webp";
+}
+
+function imageBlueberry(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/PASTEL MORA AZUL.webp";
+}
+
+function imageMint(){
+    document.getElementById("modelo3").src="../Modelo-RBL/imgColorsHair/Modelo3-RBL/PASTEL MENTA.webp";
+}
